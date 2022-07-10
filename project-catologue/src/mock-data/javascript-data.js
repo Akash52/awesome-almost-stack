7a417f537361a63e624c90b2a03736c9b200b4fa
Akash52 <ac8572611@gmail.com>
2022-07-10 14:57:54 +0530

    mock data

---
+++ project-catologue/src/mock-data/javascript-data.js
@@ -0,0 +1,81 @@
+const projects = [
+  {
+    id: 1,
+    name: 'Project 1',
+    image: 'https://picsum.photos/200/300',
+    stackblitz: 'https://stackblitz.com/edit/react-project-3',
+    live_demo: 'https://react-project-3.netlify.app/',
+    description: 'This is project 1',
+    technologies: ['React', 'Redux', 'Node'],
+    status: 'In Progress',
+    likes: 0,
+    createdAt: '2020-01-01',
+    updatedAt: '2020-01-01',
+    contributors: [
+      {
+        id: 1,
+        login: 'mojombo',
+        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+      },
+      {
+        id: 2,
+        login: 'defunkt',
+        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
+      },
+    ],
+    userId: 1,
+  },
+  {
+    id: 2,
+    name: 'Project 2',
+    image: 'https://picsum.photos/200/300',
+    stackblitz: 'https://stackblitz.com/edit/react-project-3',
+    live_demo: 'https://react-project-3.netlify.app/',
+    description: 'This is project 2',
+    technologies: ['React', 'Redux', 'Node'],
+    status: 'In Progress',
+    likes: 0,
+    createdAt: '2020-01-01',
+    updatedAt: '2020-01-01',
+    contributors: [
+      {
+        id: 1,
+        login: 'mojombo',
+        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+      },
+      {
+        id: 2,
+        login: 'defunkt',
+        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
+      },
+    ],
+    userId: 1,
+  },
+  {
+    id: 3,
+    name: 'Project 3',
+    image: 'https://picsum.photos/200/300',
+    stackblitz: 'https://stackblitz.com/edit/react-project-3',
+    live_demo: 'https://react-project-3.netlify.app/',
+    description: 'This is project 3',
+    status: 'In Progress',
+    likes: 0,
+    createdAt: '2020-01-01',
+    updatedAt: '2020-01-01',
+    contributors: [
+      {
+        id: 1,
+        login: 'mojombo',
+        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+      },
+      {
+        id: 2,
+        login: 'defunkt',
+        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
+      },
+    ],
+    userId: 1,
+  },
+];
+
+export default projects;
