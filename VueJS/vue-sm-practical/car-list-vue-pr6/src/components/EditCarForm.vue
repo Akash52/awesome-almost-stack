89e3bd6e13fa1bf486952c15a312bdf1f1d8151e
Akash52 <ac8572611@gmail.com>
2022-08-02 18:15:54 +0530

    car-list-vue-pr6

---
+++ VueJS/vue-sm-practical/car-list-vue-pr6/src/components/EditCarForm.vue
@@ -0,0 +1,146 @@
+<template>
+  <VeeForm @submit="handleSubmit">
+    <div
+      class="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none"
+    >
+      <div class="relative w-full max-w-md mx-auto my-6">
+        <div
+          class="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none"
+        >
+          <div class="relative flex-auto p-6">
+            <div>
+              <label
+                htmlFor="todo"
+                class="block mb-2 text-lg font-medium text-gray-900"
+              >
+                Add new Car
+              </label>
+              <VeeErrorMessage
+                name="name"
+                class="text-red-500 text-xs italic"
+              />
+              <VeeField
+                type="text"
+                name="name"
+                placeholder="CAR NAME"
+                class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
+                v-model="name"
+                :rules="{ required: true, min: 3, max: 20 }"
+              />
+              <VeeErrorMessage
+                name="top_speed"
+                class="text-red-500 text-xs italic"
+              />
+              <VeeField
+                type="text"
+                name="top_speed"
+                placeholder="TOP SPEED"
+                class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
+                v-model="top_speed"
+                :rules="{ required: true, numeric: true, min: 3, max: 40 }"
+              />
+              <VeeErrorMessage
+                name="description"
+                class="text-red-500 text-xs italic"
+              />
+              <VeeField
+                placeholder="DESCRIPTION"
+                name="description"
+                class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
+                v-model="description"
+                type="text"
+                :rules="{ required: true, min: 20, max: 120 }"
+              />
+              <VeeErrorMessage
+                name="image_link"
+                class="text-red-500 text-xs italic"
+              />
+              <VeeField
+                type="url"
+                name="image_link"
+                placeholder="IMAGE URL"
+                class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
+                pattern="/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i"
+                v-model="image_link"
+                :rules="{ required: true, url: true }"
+              />
+              <VeeErrorMessage
+                name="price"
+                class="text-red-500 text-xs italic"
+              />
+              <VeeField
+                type="text"
+                name="price"
+                placeholder="PRICE"
+                class="bg-gray-50 border focus:outline-none border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-2"
+                v-model="price"
+                :rules="{ required: true, numeric: true, min: 0, max: 1000000 }"
+              />
+            </div>
+          </div>
+          <div
+            class="flex items-center justify-center p-2 border-t border-solid rounded-b border-blueGray-200"
+          >
+            <button
+              class="w-full px-6 py-3 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-emerald-500 active:bg-emerald-600 hover:shadow-lg focus:outline-none"
+              type="submit"
+            >
+              UPDATE CAR
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="fixed inset-0 z-40 bg-black opacity-25"></div>
+  </VeeForm>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'EditCarForm',
+  props: ['id'],
+  // eslint-disable-next-line space-before-function-paren
+  data() {
+    return {
+      name: '',
+      top_speed: '',
+      description: '',
+      image_link: '',
+      price: ''
+    }
+  },
+
+  // eslint-disable-next-line space-before-function-paren
+  mounted() {
+    fetch(`https://vue-fake-server.herokuapp.com/cardata/${this.id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.name = data.name
+        this.top_speed = data.top_speed
+        this.description = data.description
+        this.image_link = data.image_link
+        this.price = data.price
+      })
+  },
+
+  methods: {
+    // eslint-disable-next-line space-before-function-paren
+    handleSubmit() {
+      const data = {
+        name: this.name,
+        top_speed: this.top_speed,
+        description: this.description,
+        image_link: this.image_link,
+        price: this.price
+      }
+      axios
+        .put(`https://vue-fake-server.herokuapp.com/cardata/${this.id}`, data)
+        .then((res) => {
+          this.$router.push('/')
+        })
+        .catch((err) => console.log(err))
+    }
+  }
+}
+</script>
---
+++ VueJS/vue-sm-practical/car-list-vue-pr6/src/pages/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mx-auto flex flex-col justify-center items-center mt-14">
+    <h1 class="text-2xl text-slate-800">Oops page not found 🙂</h1>
+    <p>
+      <router-link :to="{ name: 'HomePage' }">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Go to Home
+        </button>
+      </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
