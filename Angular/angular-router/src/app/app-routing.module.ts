40e34c9fec123b41661c43dc492d52ae7a0e2de9
Akash52 <ac8572611@gmail.com>
2022-09-12 15:10:43 +0530

    Angular Router

---
+++ Angular/angular-router/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+
+
+const routes: Routes = [
+
+];
+
+@NgModule({
+  imports: [
+
+  ],
+  exports: [RouterModule],
+  providers: [
+
+  ]
+})
+export class AppRoutingModule {
+
+
+}
