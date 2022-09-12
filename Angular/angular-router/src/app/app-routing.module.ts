7e51961d48dabfab0fa07f4ed9fb2a0baec22bcf
Akash52 <ac8572611@gmail.com>
2022-09-12 16:39:04 +0530

    Angular Router Essential Concepts (Routes, Paths and Outlets)

--- Angular/angular-router/src/app/app-routing.module.ts
+++ Angular/angular-router/src/app/app-routing.module.ts
@@ -1,21 +1,27 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import {
+  Routes,
+  RouterModule,
+  PreloadAllModules,
+  UrlSerializer,
+} from "@angular/router";
+import { AboutComponent } from "./about/about.component";
+import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
-
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "about",
+    component: AboutComponent,
+  },
 ];
 
 @NgModule({
-  imports: [
-
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [
-
-  ]
+  providers: [],
 })
-export class AppRoutingModule {
-
-
-}
+export class AppRoutingModule {}
