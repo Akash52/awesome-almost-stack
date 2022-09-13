f3979871b874d15bd247b354d04ced272c160126
Akash52 <ac8572611@gmail.com>
2022-09-13 11:06:27 +0530

    Angular Router Lazy Loaded Modules

--- Angular/angular-router/src/app/courses/courses-routing.module.ts
+++ Angular/angular-router/src/app/courses/courses-routing.module.ts
@@ -1,22 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-
+  {
+    path: "",
+    component: HomeComponent,
+  },
 ];
 
 @NgModule({
-  imports: [
-
-  ],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: [
-
-  ]
+  providers: [],
 })
-export class CoursesRoutingModule {
-
-
-
-}
+export class CoursesRoutingModule {}
