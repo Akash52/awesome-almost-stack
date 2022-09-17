b6235450f35ca572971a53d60f17b9f7d0371ad9
Akash52 <ac8572611@gmail.com>
2022-09-17 11:56:47 +0530

    How to write a Custom Form Field Validator Function

--- Angular/angular-forms-course/src/app/validators/password-strength-validator.ts
+++ Angular/angular-forms-course/src/app/validators/password-strength-validator.ts
@@ -1,9 +1,17 @@
 import { ValidationErrors, ValidatorFn, AbstractControl } from "@angular/forms";
 
 export function createPasswordStrengthValidator(): ValidatorFn {
-  return (control: AbstractControl) : ValidationErrors | null {
-    
-  }
-    
-  
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
+
+    if (!value) {
+      return null;
+    }
+    const hasUpperCase = /[A-Z]+/.test(value);
+    const hasLowerCase = /[a-z]+/.test(value);
+    const hasNumeric = /[0-9]+/.test(value);
+
+    const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+    return !passwordValid ? { passwordStrength: true } : null;
+  };
 }
