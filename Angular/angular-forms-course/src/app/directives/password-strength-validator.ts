8c87c5db652bd4ea9c3d0d6e7b179672928fb571
Akash52 <ac8572611@gmail.com>
2022-09-18 01:54:25 +0530

    Implementing a Custom Form Field Validation Directive

---
+++ Angular/angular-forms-course/src/app/directives/password-strength-validator.ts
@@ -0,0 +1,24 @@
+import { Directive } from "@angular/core";
+import {
+  AbstractControl,
+  ValidationErrors,
+  Validator,
+  NG_VALIDATORS,
+} from "@angular/forms";
+import { createPasswordStrengthValidator } from "../validators/password-strength-validator";
+
+@Directive({
+  selector: "[passwordStrength]",
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: PasswordStrengthDirective,
+      multi: true,
+    },
+  ],
+})
+export class PasswordStrengthDirective implements Validator {
+  validate(control: AbstractControl): ValidationErrors | null {
+    return createPasswordStrengthValidator()(control);
+  }
+}
