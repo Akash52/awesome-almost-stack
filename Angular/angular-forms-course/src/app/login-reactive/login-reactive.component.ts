d377e5ab75bec6bf264e5a4977c5f313be80d6bc
Akash52 <ac8572611@gmail.com>
2022-09-18 23:40:38 +0530

    Introduction to Reactive Forms - Step-by-Step Example

--- Angular/angular-forms-course/src/app/login-reactive/login-reactive.component.ts
+++ Angular/angular-forms-course/src/app/login-reactive/login-reactive.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login-reactive.component.html',
-  styleUrls: ['./login-reactive.component.css']
+  selector: "login",
+  templateUrl: "./login-reactive.component.html",
+  styleUrls: ["./login-reactive.component.css"],
 })
 export class LoginReactiveComponent implements OnInit {
+  form = new FormGroup({
+    email: new FormControl("", {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl("", {
+      validators: [Validators.required, Validators.minLength(8)],
+    }),
+  });
 
+  constructor() {}
 
-  constructor() {
+  ngOnInit() {}
 
-
-  }
-
-  ngOnInit() {
-
-  }
-
+  login() {}
 }
