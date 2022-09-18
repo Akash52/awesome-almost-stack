3c1e1952a85858311784983a21a96b9f1cc7777f
Akash52 <ac8572611@gmail.com>
2022-09-18 23:46:49 +0530

    The Form Control Directive and Custom Validators

--- Angular/angular-forms-course/src/app/login-reactive/login-reactive.component.ts
+++ Angular/angular-forms-course/src/app/login-reactive/login-reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { createPasswordStrengthValidator } from "./../validators/password-strength-validator";
 
 @Component({
   selector: "login",
@@ -10,9 +11,14 @@ export class LoginReactiveComponent implements OnInit {
   form = new FormGroup({
     email: new FormControl("", {
       validators: [Validators.required, Validators.email],
+      updateOn: "blur",
     }),
     password: new FormControl("", {
-      validators: [Validators.required, Validators.minLength(8)],
+      validators: [
+        Validators.required,
+        Validators.minLength(8),
+        createPasswordStrengthValidator(),
+      ],
     }),
   });
 
