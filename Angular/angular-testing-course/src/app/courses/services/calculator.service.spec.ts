8d6c599b3ffa564389aeccab277234f5516c5868
Akash52 <ac8572611@gmail.com>
2022-09-19 11:32:26 +0530

    Step-by-Step Implementation of our First Jasmine Specification

--- Angular/angular-testing-course/src/app/courses/services/calculator.service.spec.ts
+++ Angular/angular-testing-course/src/app/courses/services/calculator.service.spec.ts
@@ -1,8 +1,15 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
 describe("CalculatorService", () => {
   it("should add tow number", () => {
-    pending();
+    const calculation = new CalculatorService(new LoggerService());
+    const result = calculation.add(2, 2);
+    expect(result).toBe(4);
   });
   it("should subtract two number", () => {
-    pending();
+    const calculation = new CalculatorService(new LoggerService());
+    const result = calculation.subtract(4, 2);
+    expect(result).toBe(2, "unexpected result");
   });
 });
