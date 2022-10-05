6d15614cd1c2d9f5a90571f4563488af4f3336cb
Akash52 <ac8572611@gmail.com>
2022-10-05 11:19:20 +0530

    Angular Router Configuration - Best Practices

--- Angular/angular-router/src/app/app-routing.module.ts
+++ Angular/angular-router/src/app/app-routing.module.ts
@@ -7,8 +7,14 @@ import {
 } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { LoginComponent } from "./login/login.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/courses",
+    pathMatch: "full",
+  },
   {
     path: "courses",
     loadChildren: () =>
@@ -22,10 +28,15 @@ const routes: Routes = [
     path: "about",
     component: AboutComponent,
   },
+  {
+    path: "**",
+    component: PageNotFoundComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+
   exports: [RouterModule],
   providers: [],
 })
