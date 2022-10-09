2d276b4552b5889f423c483490e578de21415fb6
Akash52 <ac8572611@gmail.com>
2022-10-09 23:39:19 +0530

    Response Status Codes

--- NestJS/iluvcoffee/src/coffees/coffees.controller.ts
+++ NestJS/iluvcoffee/src/coffees/coffees.controller.ts
@@ -1,10 +1,18 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'This action returns all coffees';
+  findAll(@Res() response) {
+    response.status(200).send('This action returns all coffees');
   }
 
   @Get(':id')
@@ -13,6 +21,7 @@ export class CoffeesController {
   }
 
   @Post()
+  @HttpCode(201)
   create(@Body() body) {
     return body;
   }
