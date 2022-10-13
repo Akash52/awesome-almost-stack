da74aa69d5a3ca95aa1415d765634f5661fe6d72
Akash52 <ac8572611@gmail.com>
2022-10-13 09:12:04 +0530

    Learn How Observables Work Under the Hood, Build Your Own HTTP Observable

--- Angular/rxjs-in-practice/rxjs-course-1-operators/src/app/about/about.component.ts
+++ Angular/rxjs-in-practice/rxjs-course-1-operators/src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import { fromEvent, interval, timer } from "rxjs";
+import { fromEvent, interval, noop, Observable, timer } from "rxjs";
 
 @Component({
   selector: "about",
@@ -10,22 +10,38 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    const interval$ = interval(1000);
-
+    // const interval$ = interval(1000);
     // interval$.subscribe((x) => console.log("interval", x));
     // interval$.subscribe((x) => console.log("interval2", x));
-
     //Timer
-    const timer$ = timer(3000, 1000);
-    const sub = timer$.subscribe((x) => console.log("timer", x));
-
-    setTimeout(() => sub.unsubscribe(), 5000);
-
+    // const timer$ = timer(3000, 1000);
+    // const sub = timer$.subscribe((x) => console.log("timer", x));
+    // setTimeout(() => sub.unsubscribe(), 5000);
     //fromEvent
-    const click$ = fromEvent(document, "click");
-    click$.subscribe(
-      (x) => console.log("click", x),
-      (err) => console.log(err),
+    // const click$ = fromEvent(document, "click");
+    // click$.subscribe(
+    //   (x) => console.log("click", x),
+    //   (err) => console.log(err),
+    //   () => console.log("Completed")
+    // );
+
+    const http$ = Observable.create((observer) => {
+      fetch("/api/courses")
+        .then((response) => {
+          return response.json();
+        })
+        .then((body) => {
+          observer.next(body);
+          observer.complete();
+          observer.next();
+        })
+        .catch((err) => {
+          observer.error(err);
+        });
+    });
+    http$.subscribe(
+      (courses) => console.log(courses),
+      noop,
       () => console.log("Completed")
     );
   }
