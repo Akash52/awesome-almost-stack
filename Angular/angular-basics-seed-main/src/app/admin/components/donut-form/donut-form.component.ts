f8bdcd05bdb6ec2cf9fc7070450c826a89fdfd0f
Akash52 <ac8572611@gmail.com>
2022-10-22 14:33:41 +0530

    radio button binding

--- Angular/angular-basics-seed-main/src/app/admin/components/donut-form/donut-form.component.ts
+++ Angular/angular-basics-seed-main/src/app/admin/components/donut-form/donut-form.component.ts
@@ -12,10 +12,45 @@ import { Component, OnInit } from '@angular/core';
         <span> Price </span>
         <input type="number" name="price" class="input" ngModel />
       </label>
+      <div class="donut-form-radios">
+        <p class="donut-form-radios-label">Promo :</p>
+        <label>
+          <input type="radio" name="promo" value="undefined" ngModel />
+          <span> None </span>
+        </label>
+        <label>
+          <input type="radio" name="promo" value="new" ngModel />
+          <span> Yes </span>
+        </label>
+        <label>
+          <input type="radio" name="promo" value="limited" ngModel />
+          <span> No </span>
+        </label>
+      </div>
       <pre>{{ form.value | json }}</pre>
     </form>
   `,
-  styles: [],
+  styles: [
+    `
+      .donut-form {
+        &-radios {
+          display: flex;
+          align-content: center;
+          &-label {
+            margin-right: 8px;
+          }
+          label {
+            display: flex;
+            align-items: center;
+            span {
+              color: #444;
+              margin-bottom: 0;
+            }
+          }
+        }
+      }
+    `,
+  ],
 })
 export class DonutFormComponent implements OnInit {
   constructor() {}
