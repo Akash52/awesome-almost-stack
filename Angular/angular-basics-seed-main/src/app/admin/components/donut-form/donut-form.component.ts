1eb63017045efcf63722d349e23eae1719859b76
Akash52 <ac8572611@gmail.com>
2022-10-28 17:14:59 +0530

    emit form payload to smart container via output

--- Angular/angular-basics-seed-main/src/app/admin/components/donut-form/donut-form.component.ts
+++ Angular/angular-basics-seed-main/src/app/admin/components/donut-form/donut-form.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Donut } from './../../models/donut.model';
 
 @Component({
   selector: 'app-donut-form',
@@ -241,11 +242,13 @@ import { NgForm } from '@angular/forms';
   ],
 })
 export class DonutFormComponent {
+  @Output() create = new EventEmitter<Donut>();
+
   constructor() {}
 
   handleSubmit(form: NgForm) {
     if (form.valid) {
-      console.log(form.value);
+      this.create.emit(form.value);
     } else {
       form.form.markAllAsTouched();
     }
--- Angular/angular-basics-seed-main/src/app/admin/containers/donut-single/donut-single.component.ts
+++ Angular/angular-basics-seed-main/src/app/admin/containers/donut-single/donut-single.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Donut } from './../../models/donut.model';
 
 @Component({
   selector: 'app-donut-single',
   template: `
     <div>
-      <app-donut-form></app-donut-form>
+      <app-donut-form (create)="onCreate($event)"> </app-donut-form>
     </div>
   `,
   styles: [],
@@ -13,4 +14,7 @@ export class DonutSingleComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  onCreate(donut: Donut) {
+    console.log('Donut created', donut);
+  }
 }
