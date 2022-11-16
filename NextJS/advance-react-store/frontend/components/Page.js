4dec36ee89db5e45712f506a1af86ce3cd49c6bb
Akash52 <ac8572611@gmail.com>
2022-11-16 18:26:38 +0530

    Creating our Header and Nav Components

--- NextJS/advance-react-store/frontend/components/Page.js
+++ NextJS/advance-react-store/frontend/components/Page.js
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
 import PropsTypes from 'prop-types';
+import Header from './Header';
 
 const Page = ({ children }) => (
   <div>
+    <Header />
     <h2>I am the page compo</h2>
     {children}
   </div>
